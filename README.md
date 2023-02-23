4285d309cfbb90f8636df213a27ee29487fa15cb
luoChen <hank.luo@xianghuanji.com>
2023-02-23 09:55:54 +0800

    ..

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TinkerDemo
 tinker 源码阅读 基于 v1.9.14.25.1  tag
+
+运行项目里的 tinker-simple-android项目
---
+++ tinker-1.9.14.25.1/tinker-android/mysimple/src/main/java/com/niluogege/mysimple/MainActivity.java
@@ -0,0 +1,17 @@
+package com.niluogege.mysimple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tencent.tinker.lib.tinker.Tinker;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
